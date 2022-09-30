3bfd67eaac0e71ab3b9c43fd2a170b7d4bd5e483
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-09-30 18:01:32 +0530

    Add files via upload

--- upload.php
+++ upload.php
@@ -2,31 +2,39 @@
 	<form action="" method="POST" enctype="multipart/form-data">
 		<input type ="text" name ="txtroll" placeholder ="enterrollno."></br>
 		<input type ="text" name ="txtnm" placeholder ="enter name"></br>
-		<input type ="file" name ="" accept =".png, .jpg, .jpeg"</br>
-		<img scr="" alt ="select image" height ="150" width ="150"></br>
+		<input type ="file" name ="janvi" id ="imgInp" accept =".png, .jpg, .jpeg"</br>
+		<img scr="" id ="blah" alt ="select image" height ="150" width ="150"></br>
 		<input type ="submit" value ="save">
 	</form>
 
-	<?php
-		if(isset($_POST['txtroll']))
+	  <?php
+		if(isset($_POST['txtroll'])){
+			$con=new mysqli("localhost","root","","janvi");
+			if($con){
+			$rnm=$_POST['txtroll'];
+			$nm=$_POST['txtnm'];
+			$target_div="janvi/";
+			$name=rand(150,148000);
+			$extension = pathinfo($_FILES["janvi"]["name"],PATHINFO_EXTENSION);
+			$fnm=$name.$extension;
+			move_uploaded_file($_FILES["janvi"]["tmp_name"],$target_div.$name.".".$extension);
+			$sql="INSERT INTO `img`(`rollno`, `name`, `file`) VALUES ('$rnm','$nm','$fnm')";
+			mysqli_query($con,$sql);
+		}
+	}
+	?>
+  <script>
+	imgInp.onchange = evt =>
 		{
-			$con = mysqli_connect("localhost","root",""," ");
-			if($con)
+			const [file] = imgInp.files
+				if (file)
 			{
-				$roll = $_POST['txtroll'];
-				$nm = $_POST['txtnm'];
-				$target_dir ="/";
-				$name = rand(150-460000);
-				$extension = pathinfo($_FILES["upload1"]["name"],PATHINFO_EXTENSION);
-				$fnm = $name.$extension;
-				move_uploaded_file($_FILES[""][tmp_name],$target_dir.$name.".".$extension);
-				$sql = 
-				mysqli_query()
-				
+				blah.src = URL.createObjectURL(file)
 			}
 		}
-	?>
-	
+</script>
 
-</html>
+
+
+
 	 
